cf10324ef70c1251d77ed25bc2364f7124a61c26
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-08-09 23:18:47 +0100

    Add an initial representation of finger trees.

--- src/main/scala/FingerTree.scala
+++ src/main/scala/FingerTree.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable.ListBuffer
+
 trait Tree[A]
 case class Zero[A](a: A) extends Tree[A]
 case class Succ[A](a: Tree[Node[A]]) extends Tree[A]
@@ -7,15 +9,22 @@ case class Node2[A](a1: A, a2: A) extends Node[A]
 case class Node3[A](a1: A, a2: A, a3: A) extends Node[A]
 
 object Node {
-  def apply[A](a1: A, a2: A) = Node2(a1, a2)
-  def apply[A](a1: A, a2: A, a3: A) = Node3(a1, a2, a3)
+  def apply[A](a1: A, a2: A): Node[A] = Node2(a1, a2)
+  def apply[A](a1: A, a2: A, a3: A): Node[A] = Node3(a1, a2, a3)
 }
+
+trait FingerTree[A]
+case class Empty[A] extends FingerTree[A]
+case class Single[A](o: A) extends FingerTree[A]
+case class Deep[A](dl: Digit[A], t: FingerTree[Node[A]], dr: Digit[A])
+
+case class Digit[A](a: ListBuffer[A])
 
 object Main extends App {
   val t = Succ(Zero(Node(Succ(Zero(Node(Succ(Zero(Node('t', 'h'))),
                                         Succ(Zero(Node('i', 's'))),
                                         Succ(Zero(Node('i', 's')))))),
-                         Succ(Zero(Node(Succ(Zero(Node('n', 'o', 't')))
+                         Succ(Zero(Node(Succ(Zero(Node('n', 'o', 't'))),
                                         Succ(Zero(Node('a', 't'))),
                                         Succ(Zero(Node('r', 'e', 'e')))))))))
 }
