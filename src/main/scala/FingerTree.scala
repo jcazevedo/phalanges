9e65fe0f2be7ad68ca61ef1fed9bfaca29f575fb
Joao Azevedo <joao.c.azevedo@gmail.com>
2012-08-09 23:56:44 +0100

    Add a representation of the same tree as a finger tree.

--- src/main/scala/FingerTree.scala
+++ src/main/scala/FingerTree.scala
@@ -13,12 +13,16 @@ object Node {
   def apply[A](a1: A, a2: A, a3: A): Node[A] = Node3(a1, a2, a3)
 }
 
-trait FingerTree[A]
+trait FingerTree[-A]
 case class Empty[A] extends FingerTree[A]
 case class Single[A](o: A) extends FingerTree[A]
-case class Deep[A](dl: Digit[A], t: FingerTree[Node[A]], dr: Digit[A])
+case class Deep[A](dl: Digit[A], t: FingerTree[Node[A]], dr: Digit[A]) extends FingerTree[A]
 
-case class Digit[A](a: ListBuffer[A])
+class Digit[+A](a: ListBuffer[A])
+
+object Digit {
+  def apply[A](v: A*) = new Digit(ListBuffer(v:_*))
+}
 
 object Main extends App {
   val t = Succ(Zero(Node(Succ(Zero(Node(Succ(Zero(Node('t', 'h'))),
@@ -27,4 +31,10 @@ object Main extends App {
                          Succ(Zero(Node(Succ(Zero(Node('n', 'o', 't'))),
                                         Succ(Zero(Node('a', 't'))),
                                         Succ(Zero(Node('r', 'e', 'e')))))))))
+
+  val f = Deep(Digit('i', 'h'),
+               Deep(Digit(Node('i', 's'), Node('i', 's')),
+                    Empty[Node[Any]],
+                    Digit(Node('n', 'o', 't'), Node('a', 't'))),
+               Digit('r', 'e', 'e'))
 }
