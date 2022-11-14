8d77d2d34363b0fd7a729c64a06541b03a5004ae
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-11-14 00:31:59 +0000

    Avoid explicit requirements for `Monoid` in `Node`'s smart constructors

--- src/main/scala/net/jcazevedo/phalange/Node.scala
+++ src/main/scala/net/jcazevedo/phalange/Node.scala
@@ -12,12 +12,9 @@ private[phalange] object Node {
   private[phalange] case class Node2[V, +A](v: V, a: A, b: A) extends Node[V, A]
   private[phalange] case class Node3[V, +A](v: V, a: A, b: A, c: A) extends Node[V, A]
 
-  private[phalange] def node2[V, A](a: A, b: A)(implicit measured: Measured[A, V], monoid: Monoid[V]): Node[V, A] =
-    Node.Node2(monoid.append(measured.apply(a), measured.apply(b)), a, b)
+  private[phalange] def node2[V, A](a: A, b: A)(implicit measured: Measured[A, V]): Node[V, A] =
+    Node.Node2(measured.append(measured.apply(a), measured.apply(b)), a, b)
 
-  private[phalange] def node3[V, A](a: A, b: A, c: A)(implicit
-      measured: Measured[A, V],
-      monoid: Monoid[V]
-  ): Node[V, A] =
-    Node.Node3(monoid.append(monoid.append(measured.apply(a), measured.apply(b)), measured.apply(c)), a, b, c)
+  private[phalange] def node3[V, A](a: A, b: A, c: A)(implicit measured: Measured[A, V]): Node[V, A] =
+    Node.Node3(measured.append(measured.append(measured.apply(a), measured.apply(b)), measured.apply(c)), a, b, c)
 }
