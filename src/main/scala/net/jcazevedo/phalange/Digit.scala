d1c0b68fca0253df74aec1cb6a65d1a9cadf90bc
Joao Azevedo <joao.c.azevedo@gmail.com>
2022-11-26 17:47:24 +0000

    Cache measurements in Digit

--- src/main/scala/net/jcazevedo/phalange/Digit.scala
+++ src/main/scala/net/jcazevedo/phalange/Digit.scala
@@ -1,18 +1,88 @@
 package net.jcazevedo.phalange
 
-private[phalange] sealed trait Digit[+A] extends Iterable[A] {
-  def iterator: Iterator[A] =
-    this match {
-      case Digit.One(a)           => Iterator(a)
-      case Digit.Two(a, b)        => Iterator(a, b)
-      case Digit.Three(a, b, c)   => Iterator(a, b, c)
-      case Digit.Four(a, b, c, d) => Iterator(a, b, c, d)
-    }
+private[phalange] sealed abstract class Digit[V, +A] extends Iterable[A] {
+  private[phalange] def fold[B](
+      one: (Lazy[V], A) => B,
+      two: (Lazy[V], A, A) => B,
+      three: (Lazy[V], A, A, A) => B,
+      four: (Lazy[V], A, A, A, A) => B
+  ): B
+
+  final def measure: V =
+    fold(
+      one = (lm, _) => lm.value,
+      two = (lm, _, _) => lm.value,
+      three = (lm, _, _, _) => lm.value,
+      four = (lm, _, _, _, _) => lm.value
+    )
+
+  final def iterator: Iterator[A] =
+    fold(
+      one = (_, a) => Iterator(a),
+      two = (_, a, b) => Iterator(a, b),
+      three = (_, a, b, c) => Iterator(a, b, c),
+      (_, a, b, c, d) => Iterator(a, b, c, d)
+    )
 }
 
 private[phalange] object Digit {
-  private[phalange] case class One[A](a: A) extends Digit[A]
-  private[phalange] case class Two[A](a: A, b: A) extends Digit[A]
-  private[phalange] case class Three[A](a: A, b: A, c: A) extends Digit[A]
-  private[phalange] case class Four[A](a: A, b: A, c: A, d: A) extends Digit[A]
+  private[phalange] def apply[V, A](a: A)(implicit measured: Measured[A, V]): Digit[V, A] =
+    new Digit[V, A] {
+      def fold[B](
+          one: (Lazy[V], A) => B,
+          two: (Lazy[V], A, A) => B,
+          three: (Lazy[V], A, A, A) => B,
+          four: (Lazy[V], A, A, A, A) => B
+      ): B =
+        one(new Lazy(measured.apply(a)), a)
+    }
+
+  private[phalange] def apply[V, A](a: A, b: A)(implicit measured: Measured[A, V]): Digit[V, A] =
+    new Digit[V, A] {
+      def fold[B](
+          one: (Lazy[V], A) => B,
+          two: (Lazy[V], A, A) => B,
+          three: (Lazy[V], A, A, A) => B,
+          four: (Lazy[V], A, A, A, A) => B
+      ): B =
+        two(new Lazy(measured.append(measured.apply(a), measured.apply(b))), a, b)
+    }
+
+  private[phalange] def apply[V, A](a: A, b: A, c: A)(implicit measured: Measured[A, V]): Digit[V, A] =
+    new Digit[V, A] {
+      def fold[B](
+          one: (Lazy[V], A) => B,
+          two: (Lazy[V], A, A) => B,
+          three: (Lazy[V], A, A, A) => B,
+          four: (Lazy[V], A, A, A, A) => B
+      ): B =
+        three(
+          new Lazy(measured.append(measured.apply(a), measured.append(measured.apply(b), measured.apply(c)))),
+          a,
+          b,
+          c
+        )
+    }
+
+  private[phalange] def apply[V, A](a: A, b: A, c: A, d: A)(implicit measured: Measured[A, V]): Digit[V, A] =
+    new Digit[V, A] {
+      def fold[B](
+          one: (Lazy[V], A) => B,
+          two: (Lazy[V], A, A) => B,
+          three: (Lazy[V], A, A, A) => B,
+          four: (Lazy[V], A, A, A, A) => B
+      ): B =
+        four(
+          new Lazy(
+            measured.append(
+              measured.apply(a),
+              measured.append(measured.apply(b), measured.append(measured.apply(c), measured.apply(d)))
+            )
+          ),
+          a,
+          b,
+          c,
+          d
+        )
+    }
 }
--- src/main/scala/net/jcazevedo/phalange/Measured.scala
+++ src/main/scala/net/jcazevedo/phalange/Measured.scala
@@ -7,40 +7,8 @@ trait Measured[-A, V] extends Monoid[V] {
 object Measured {
   private[phalange] implicit def nodeMeasured[A, V](implicit monoid: Monoid[V]): Measured[Node[V, A], V] =
     new Measured[Node[V, A], V] {
-      def apply(a: Node[V, A]): V =
-        a.measure
-
-      def empty: V =
-        monoid.empty
-
-      def append(a: V, b: V) =
-        monoid.append(a, b)
+      def apply(a: Node[V, A]): V = a.measure
+      def empty: V = monoid.empty
+      def append(a: V, b: V) = monoid.append(a, b)
     }
-
-  private[phalange] implicit def digitMeasured[A, V](implicit measured: Measured[A, V]): Measured[Digit[A], V] =
-    new Measured[Digit[A], V] {
-      def apply(a: Digit[A]): V =
-        a match {
-          case Digit.One(a) =>
-            measured.apply(a)
-
-          case Digit.Two(a, b) =>
-            append(measured.apply(a), measured.apply(b))
-
-          case Digit.Three(a, b, c) =>
-            append(append(measured.apply(a), measured.apply(b)), measured.apply(c))
-
-          case Digit.Four(a, b, c, d) =>
-            append(append(append(measured.apply(a), measured.apply(b)), measured.apply(c)), measured.apply(d))
-        }
-
-      def empty: V =
-        measured.empty
-
-      def append(a: V, b: V) =
-        measured.append(a, b)
-    }
-
-  private[phalange] def measure[A, V](a: A)(implicit measured: Measured[A, V]): V =
-    measured.apply(a)
 }
